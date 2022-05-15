371ab064290d947e871ae2b9e9012e0f938f2b73
EvgenyVarlamov <varlamov-el@yandex.ru>
2022-05-15 15:46:22 +0300

    add Additional homework #3 Mix of array elements Shuffle()

---
+++ Additional_Hometask003/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Написать функцию Shuffle, которая перемешивает элементы массива в случайном порядке. */
+
+
+string[] text = {"функкция", "перемешивает", "элементы", "массива", "в", "случайном", "порядке"};
+
+Console.WriteLine();
+PrintArray(text);
+Console.WriteLine();
+Shuffle(text);
+Console.WriteLine();
+
+void PrintArray(string[] arr)
+{
+    Console.Write("Массив: ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void Shuffle(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int index = new Random().Next(0, array.Length);
+        string temp = array[i];
+        array[i] = array[index];
+        array[index] = temp;
+    }
+    PrintArray(array);
+}
